8c550892f2a7db48240b695bd2cba7cda40d1db6
jamessch16 <jamessch16@gmail.com>
2025-10-23 00:19:25 -0600

    results reporting

--- a3p1.c
+++ a3p1.c
@@ -8,7 +8,10 @@ typedef struct PageReference {
     bool dirty;
 } PageReference;
 
-
+typedef struct SimulationResults {
+    int page_faults = 0;
+    int write_backs = 0;
+} SimulationResults;
 
 void read_input(PageReference page_references []);
 void next_line(FILE *filestream);
@@ -51,7 +54,7 @@ void next_line(FILE *filestream) {
 }
 
 
-void simulate_fifo(PageReference page_references[], int frame_count) {
+SimulationResults simulate_fifo(PageReference page_references[], int frame_count) {
 
     int page_faults = 0;
     int write_backs = 0;
@@ -65,45 +68,88 @@ void simulate_fifo(PageReference page_references[], int frame_count) {
 }
 
 
-void simulate_optimal(PageReference page_references[], int frame_count) {
+SimulationResults simulate_optimal(PageReference page_references[], int frame_count) {
     
 }
 
-void simulate_clock(PageReference page_references[], int reference_bits, int interrupt_period) {
+SimulationResults simulate_clock(PageReference page_references[], int num_reference_bits, int interrupt_period) {
 
+    int reference_bits[NUM_PAGES] = {0};
 }
 
 
 int main(int argc, char *argv[]) {
 
     PageReference page_references[NUM_REFERENCES];
+    SimulationResults sim_results;
 
     read_input(page_references);
 
+
+
+
+    // FIFO simulation
     if (strcmp(argv[1], "FIFO") == 0) {
+
+        // print output header
+        printf("FIFO\n");
+        printf("+-----------+-----------------+-----------------+\n");
+        printf("+  Frames   +   Page Faults   +   Write-Backs   +\n");
+        printf("+-----------+-----------------+-----------------+\n");
+
         for (int frame_count = 1; frame_count <= 100; frame_count++) {
-            simulate_fifo(page_references, frame_count);
+            sim_results = simulate_fifo(page_references, frame_count);
+            printf("+%10d +%16d +%16d +\n", frame_count, sim_results.page_faults, sim_results.write_backs);
+            printf("+-----------+-----------------+-----------------+\n");
         }
     }
     
+    // Optimal simulation
     if (strcmp(argv[1], "OPT") == 0) {
+
+        // print output header
+        printf("OPT\n");
+        printf("+-----------+-----------------+-----------------+\n");
+        printf("+  Frames   +   Page Faults   +   Write-Backs   +\n");
+        printf("+-----------+-----------------+-----------------+\n");
+        
         for (int frame_count = 1; frame_count <= 100; frame_count++) {
-            simulate_optimal(page_references, frame_count);
+            sim_results = simulate_optimal(page_references, frame_count);
+            printf("+%10d +%16d +%16d +\n", frame_count, sim_results.page_faults, sim_results.write_backs);
+            printf("+-----------+-----------------+-----------------+\n");
         }
     }
 
+    // Clock simulation
     if (strcmp(argv[1], "CLK") == 0) {
+
+        // print output header
+        printf("CLK, m = 10\n");
+        printf("+-----+-----------------+-----------------+\n");
+        printf("+  n  +   Page Faults   +   Write-Backs   +\n");
+        printf("+-----+-----------------+-----------------+\n");
+        
         // vary reference bits, fixing interrupt period = 10
-        for (int reference_bits = 1; reference_bits <= 32; reference_bits++) {
-            simulate_clock(page_references, reference_bits, 10);
+        for (int n_reference_bits = 1; n_reference_bits <= 32; n_reference_bits++) {
+            sim_results = simulate_clock(page_references, n_reference_bits, 10);
+            printf("+%4d +%16d +%16d +", n_reference_bits, sim_results.page_faults, sim_results.write_backs);
+            printf("+-----+-----------------+-----------------+\n");
         }
+
+
+        // print output header
+        printf("\n\nCLK, m = 10\n");
+        printf("+-----+-----------------+-----------------+\n");
+        printf("+  m  +   Page Faults   +   Write-Backs   +\n");
+        printf("+-----+-----------------+-----------------+\n");
 
         // vary interrupt period, fixing reference bits = 8
-        for (int interrupt_period = 1; interrupt_period <= 100; interrupt_period++) {
-            simulate_clock(page_references, 8, interrupt_period);
+        for (int m_interrupt_period = 1; m_interrupt_period <= 100; m_interrupt_period++) {
+            sim_results = simulate_clock(page_references, 8, m_interrupt_period);
+            printf("+%4d +%16d +%16d +\n", m_interrupt_period, sim_results.page_faults, sim_results.write_backs);
+            printf("+-----+-----------------+-----------------+\n");
         }
     }
-
 
     return 0;
 }
